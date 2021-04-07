16c3908f7d9b27dc62cede0437ce5b429a8d4056
younger15 <32859361+younger15@users.noreply.github.com>
2021-04-07 12:53:15 +0800

    Init Authenticator header

---
+++ src/Header/Authenticator.h
@@ -0,0 +1,30 @@
+// Copyright (c) 2021, 鍾淯丞, 周杰仕, 林仁鴻. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef GROUP4_HEADER_AUTHENTICATOR_H_
+#define GROUP4_HEADER_AUTHENTICATOR_H_
+
+#include <string>
+
+//used to check if request is send from valid user
+//when getting request, requesthandler call authenticator::CheckAccount(account, password) check if it's valid.
+//if return true then keep handle request, false will deny the request.
+class Authenticator
+{
+public:
+    //singleton code convention method.
+    static Authenticator* GetInstance();
+    //check if login is correct
+    bool CheckAccount(const std::string &account, const std::string &password);
+    //check if account exist
+    std::string SearchAccount(const std::string &account);
+    //non copyable
+    Authenticator(Authenticator& cpy) = delete;
+    Authenticator& operator=(Authenticator& cpy) = delete;
+private:
+    //singleton code convention method.
+    Authenticator();
+};
+
+#endif
