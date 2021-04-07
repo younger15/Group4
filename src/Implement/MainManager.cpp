533874796c22419b82b89f0de2a582765041cef9
younger15 <32859361+younger15@users.noreply.github.com>
2021-04-07 12:53:15 +0800

    Init MainManager implement

---
+++ src/Implement/MainManager.cpp
@@ -0,0 +1,85 @@
+#include "MainManager.h"
+
+MainManager *MainManager::only_mainManager = nullptr;
+
+namespace {
+void SigHandle(int sig, siginfo_t *info, void *ctx) {
+  if (sig == SIGUSR1) {
+    mainManager.RemoveByPid(info->si_pid);
+  }
+}
+}  // namespace
+
+void MainManager::SetRequestHandler(const RequestHandlerInterface &request_handler){
+  this->request_handler = request_handler;
+}
+
+void MainManager::InitInstance(const uint16_t &port_num) {
+  this->port_num = port_num;
+  // when child process ends, it'll exit itself and need to notify parent process to remove it from socket_map
+  // child process will end with calling EndRequest()
+  struct sigaction act;
+  act.sa_sigaction = SigHandle;
+  sigemptyset(&act.sa_mask);
+  act.sa_flags = SA_SIGINFO;
+  sigaction(SIGUSR1, &act, NULL);
+  request_handler = RequestHandler();
+  HttpListener listen_socket = HttpListener::GetInstance();
+  listen_socket.InitInstance(this->NewRequest, port_num);
+  listen_socket.StartListen();
+}
+
+MainManager *MainManager::GetInstance() {
+  if (only_mainManager == nullptr) {
+    only_mainManager = new MainManager();
+  }
+  return only_mainManager;
+}
+
+void MainManager::NewRequest(const int &socket_fd) {
+  pid_t child_pid = fork();
+  // means parent
+  if (child_pid > 0) {
+    socket_map[socket_fd] = child_pid;
+  }
+  // means child
+  else if (child_pid == 0) {
+    request_handler.SetFd(socket_fd);
+    request_handler.WaitForMessage();
+  }
+  // fork failed
+  else {
+    // TODO: call error
+  }
+}
+
+void MainManager::EndRequest() { kill(getppid(), SIGUSR1); }
+
+void MainManager::RemoveBySocket(const int &socket_num) {
+  std::unordered_map<int, pid_t>::const_iterator find_process =
+      socket_map.find(socket_num);
+  if (got == tmpInt.end()) {
+    // TODO: call error
+  } else {
+    socket_map.erase(find_process);
+  }
+}
+
+void MainManager::RemoveByPid(const pid_t &p) {
+  for (std::unordered_map<int, pid_t>::iterator find_process =
+           socket_map.begin();
+       find_process != tmpInt.end();) {
+    if (i->second == p) {
+      find_process = socket_map.erase(find_process);
+    } else {
+      ++find_process;
+      if (find_process == socket_map.end()) {
+        // TODO: call error
+      }
+    }
+  }
+}
+
+pid_t MainManager::GetProcess(int socket_num) { return socket_map[socket_num]; }
+
+int MainManager::GetSize() { return socket_map.size(); }
