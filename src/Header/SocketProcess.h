8744e96af68792a898add7e0c3c1b9b70cd661f5
Jazz <winsten.chou@gmail.com>
2021-04-12 18:09:44 +0800

    init commit:

---
+++ src/Header/SocketProcess.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2021, 鍾淯丞, 周杰仕, 林仁鴻. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <sys/socket.h>
+
+class SocketProcess
+{
+public:
+private:
+    void StartProc();
+}
