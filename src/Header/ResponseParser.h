a8de17e9af90a6cedd0b7ae44482b11e9e639657
林仁鴻 <appleken51688@gmail.com>
2021-04-27 20:28:44 +0800

    Initial commit for ResponseParser.h and ResponseParser.cpp

--- src/Header/ResponseParser.h
+++ src/Header/ResponseParser.h
@@ -10,28 +10,36 @@
 
 #include "OperateData.h"
 
-// Used to make request string to OperateData(JSON like) format.
-// string responseStr = "[X: X_DATA, Y: Y_DATA]"
-// unique_ptr<OperateData> data = RequestParser::ParserHeader(responseStr)
-// data will return header and body.
+/*
+    // Used to make request string to OperateData format.
+    string responseStr = "HTTP/1.1 201 Created
+                          Server: libnhttpd
+                          Date: Wed Jul 4 15:31:53 2012
+                          Connection: Keep-Alive
+                          Content-Type: application/json;charset=UTF-8
+                          Content-Length: 304
+                          {
+                              packet-num: 5
+                          }"
+    ResponseParser* response_parser = ResponseParser::GetInstance();
+    std::unique_ptr<OperateData> result = response_parser->ParseData(test_string);
+*/
+
 class ResponseParser {
- public:
-  // Singleton code convention method
-  static RequestParser *GetInstance();
-
-  // Give request string return header
-  std::unique_ptr<OperateData> ParseHeader(std::string &response);
-
-  // Give request string return body
-  std::unique_ptr<OperateData> ParseBody(std::string &response);
-
-  // Not copyable or movable
-  ResponseParser(const ResponseParser &copy) = delete;
-  ResponseParser &operator=(const ResponseParser &copy) = delete;
-
- private:
-  // Singleton code convention method
-  ResponseParser();
+public:
+    // Singleton code convention method
+    static ResponseParser* GetInstance();
+    
+    // Give request string return header
+    std::unique_ptr<OperateData> ParseData(std::string &response);
+    
+    // Not copyable or movable
+    ResponseParser(const ResponseParser &copy) = delete;
+    ResponseParser& operator = (const ResponseParser &copy) = delete;
+    
+private:
+    // Singleton code convention method
+    ResponseParser();
 };
 
 #endif /* GROUP4_SRC_RESPONSEPARSER_H_ */
