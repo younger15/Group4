7d6e285196e47a0e40d2c856b142575640e45d68
younger15 <32859361+younger15@users.noreply.github.com>
2021-05-20 14:14:18 +0800

    Make Handler receive request

--- src/Header/RequestHandler.h
+++ src/Header/RequestHandler.h
@@ -9,22 +9,31 @@
 #include <string>
 #include <unordered_map>
 
-#include "OperateData.h"
-#include "Authenticator.h"
 #include "APIHandler.h"
+#include "Authenticator.h"
+#include "OperateData.h"
 
+// Will decode the request string and check if it's a valid request.
+// calling RequestParser to get OperateData then call correspond api function
+class RequestHandler {
+ public:
+  // instantiate instance with accepted file descriptor, will receive request
+  // string and parse to OperateData, then check with Authenticator and call
+  // correspond api.
+  RequestHandler(const int &file_descriptor);
+  // receive request string and send to api
+  void WaitForMessage();
+  // Get client file descriptor
+  int GetClientFd();
+  // content of request
+  std::unique_ptr<OperateData> data;
+  // storing all feature api
+  static const std::unordered_map<std::string, APIHandler &> apis;
 
-//Will decode the request string and check if it's a valid request.
-//calling RequestParser to get OperateData then call correspond api function
-class RequestHandler
-{
-public:
-    //instantiate instance with request, will parse to OperateData and check with Authenticator and call correspond api.
-    RequestHandler(const std::string &requestData);
-    //content of request
-    std::unique_ptr<OperateData> data;
-    //storing all feature api
-    static const std::unordered_map<std::string, APIHandler&> apis;
+ private:
+  // file descriptor from httpListener, which will keep receiving request from
+  // the same connected user
+  int client_fd;
 };
 
 #endif
