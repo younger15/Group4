cab47ae24e385330d7cb5cd6b495a7467051b389
林仁鴻 <appleken51688@gmail.com>
2021-06-13 23:58:21 +0800

    Inintal commit for ResponseEncoder.h and ResponseEncoder.cpp and add unit test

--- src/Header/ResponseEncoder.h
+++ src/Header/ResponseEncoder.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef GROUP4_SRC_RESPONSEENCODER_H_
-#define GROUP4_SRC_RESPONSEENCODER_H_
+#ifndef GROUP4_SRC_HEADER_RESPONSEENCODER_H_
+#define GROUP4_SRC_HEADER_RESPONSEENCODER_H_
 
 #include <memory>
 #include <string>
@@ -11,21 +11,22 @@
 #include "OperateData.h"
 
 // Encode the response
-class ResponseEncoder {
- public:
-  // Singleton code convention method.
-  static ResponseEncoder *GetInstance();
-
-  // Encode head and body information
-  std::string Encode(const OperatorData &head, const OperatorData &body);
-
-  // Not copyable or movable
-  ResponseEncoder(const ResponseEncoder &copy) = delete;
-  ResponseEncoder &operator=(const ResponseEncoder &copy) = delete;
-
- private:
-  // Singleton code convention method
-  ResponseEncoder();
+class ResponseEncoder
+{
+public:
+    // Singleton code convention method.
+    static ResponseEncoder* GetInstance();
+    
+    // Encode head and body information
+    std::string Encode(const std::unique_ptr<OperateData>& data);
+    
+    // Not copyable or movable
+    ResponseEncoder(const ResponseEncoder &copy) = delete;
+    ResponseEncoder& operator = (const ResponseEncoder &copy) = delete;
+  
+private:
+    // Singleton code convention method
+    ResponseEncoder();
 };
 
-#endif /* GROUP4_SRC_RESPONSEENCODER_H_ */
+#endif /* GROUP4_SRC_HEADER_RESPONSEENCODER_H_ */
